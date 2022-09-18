9a28d7485b167c8430d28d08b0d4686fc38aeb2d
GevManukyan <gevmanukyan110@gmail.com>
2022-09-18 20:22:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+
+let div = document.getElementById("div1")
+let ok = document.getElementById("ok")
+let div2 = document.querySelector(".div2")
+let div3 = document.querySelector(".div3")
+
+function init()
+    {
+        sec = 0
+        setInterval(tick1, 1);
+    }
+
+    function tick1(){
+       sec = sec + (Math.random()*4);
+        div.style.margin = `0px ${sec}px`;
+    }  
+    function ini()
+    {
+        sec1 = 0;
+        setInterval(tick, 1);
+    }
+    function tick(){
+       sec1 = sec1+(Math.random()*4);
+        div2.style.margin = `0px ${sec1}px`;
+    }  
+    function ini7()
+    {
+        sec2 = 0;
+        setInterval(tick7, 1);
+    }
+    function tick7(){
+       sec2 = sec2+(Math.random()*4);
+        div3.style.margin = `0px ${sec2}px`;
+    }  
+
+let win = document.querySelector(".win")
+let two =document.querySelector(".two")
+let tree = document.querySelector(".tree")
+ok.addEventListener("click",function(){ 
+    ok.style.display ="none"
+    ini()
+    ini7()
+    init()
+    setTimeout("location.reload(true);; ",7000);
+   setTimeout(() => {
+    let x = div.getBoundingClientRect().left
+    let y = div2.getBoundingClientRect().left
+    let z = div3.getBoundingClientRect().left
+    if(x>y && x>z && y>z){
+        win.innerHTML = "RED WIN"
+        win.style.color = "red"
+        two.innerHTML = "2.Yellow"
+        tree.innerHTML = "3.Blue"
+    }else if(x>y && x>z && y<z){
+        win.innerHTML = "RED WIN"
+        win.style.color = "red"
+        two.innerHTML = "2.Blue"
+        tree.innerHTML = "3.Yellow"
+    }
+    else if(y>z && y>x && z<x){
+        win.innerHTML = "YELLOW WIN";
+        win.style.color = "yellow"
+        two.innerHTML = "2.Red"
+        tree.innerHTML = "3.Blue"
+    }
+    else if(y>z && y>x && z>x){
+        win.innerHTML = "YELLOW WIN";
+        win.style.color = "yellow"
+        two.innerHTML = "2.Blue"
+        tree.innerHTML = "3.Red"
+    }else if(z>x && z>y && y<x){
+        win.innerHTML = "BLUE WIN"
+        win.style.color = "blue"
+        two.innerHTML = "2.Red"
+        tree.innerHTML = "3.Yellow"
+    }
+    else if(z>x && z>y && y>x){
+        win.innerHTML = "BLUE WIN"
+        win.style.color = "blue"
+        two.innerHTML = "2.Yellow"
+        tree.innerHTML = "3.Red"
+    }
+}, 4300);
+})    
+
